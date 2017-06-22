dd107618594942bc81fe981b815374d4c155933a
Mariana <defreitasferreira.mariana@gmail.com>
2017-06-22 20:10:56 -0300

    bolhas se movendo

--- SBA.py
+++ SBA.py
@@ -1,32 +1,28 @@
 from PPlay.window import *
 from PPlay.sprite import *
 from PPlay.gameimage import *
-from random import randint
-
-window = Window(1280, 840)
-window.set_title("Soap Bubbles Attack")
-mouse=Window.get_mouse()
-teclado=Window.get_keyboard()
-
-game_state=0
 
 window = Window(1280, 840)
 #windowmenu= Window(640,405)
 window.set_title("Soap Bubbles Attack")
-mouse=Window.get_mouse()
-teclado=Window.get_keyboard()
+mouse = Window.get_mouse()
+keyboard = window.get_keyboard()
 
-game_state=0
-
+game_state=1
 
 #Menu
-start=GameImage("start.png")
-start.x=window.width/2-start.width/2
-start.y=1*window.height/4-start.height/2
+#menu=GameImage("Menu.jpg")
+#menu1.x=window.width/2-menu1.width/2
+#menu1.y=window.height/2-menu1.height/2
+#bolhas=Sprite("bolhas.gif")
+
+
+#start=GameImage("start.png")
+#start.x=windowmenu.width/2-start.width/2
+#start.y=windowmenu.height/2-start.height/2
 
 #Mapa do jogo
-background = GameImage("background.png")
-caminho= GameImage("caminho.png")
+background = GameImage("background_line.png")
 
 #Animais para compra
 bird_c = GameImage("bird.png")
@@ -38,10 +34,21 @@ bird = Sprite("bird.png")
 llama = Sprite("llama.png")
 elephant = Sprite("elephant.png")
 
-#Torre a ser defendida
-eye=Sprite("eye.png")
+#Demonstração
+bird.x = 17*window.width/21 - bird.width
+bird.y = 8*window.height/21
+elephant.x = 8*window.width/21
+elephant.y = 10*window.height/21
 
-#Posição animais de compra.
+#Torre a ser defendida
+eye = Sprite("eye.png")
+
+#bolhas
+b1 = Sprite("b1.png")
+b2 = Sprite("b2.png")
+b3 = Sprite("b3.png")
+
+#Posição animais de compra
 bird_c.x = window.width / 10
 bird_c.y = window.height - bird_c.height
 llama_c.x = 2 * window.width / 10
@@ -49,92 +56,69 @@ llama_c.y = window.height - llama_c.height
 elephant_c.x = 3 * window.width / 10
 elephant_c.y = window.height - elephant_c.height
 
-#Posição da base.
-eye.x=window.width - eye.width
-eye.y=18*(window.height/21)  - eye.height
+#Posição da base
+eye.x = window.width - eye.width
+eye.y = window.height/2 - eye.height
 
-#bolhas
-b1=Sprite("b1.png")
-b2=Sprite("b2.png")
-b3=Sprite("b3.png")
+speed = 200
 
-b1.x=window.width/21 - b1.width
-b1.y=4*window.height/21
-wave1=10
+bolhas1 = []
+bolhas2 = []
+bolhas3 = []
+t = 1
+delta = window.delta_time()
 
-speed_x=100
-speed_y=100
-t=1
+def cria_bolhas(bolha, grupo, tipo):
+    global t
 
-h=1
+    if t >= 2:
+        t = 0
 
+        dicionario_tipos = {1: "b1.png", 2: "b2.png", 3: "b3.png"}[tipo]
+
+        bolha = Sprite(dicionario_tipos)
+        bolha.set_position(-b3.width, window.height/2 - bolha.height)
+
+        grupo.append(bolha)
+
+    for i in grupo:
+        i.x += speed * window.delta_time()
+        if i.x > window.width -3*bolha.width:
+            grupo.remove(i)
+
+    t += window.delta_time()
+    
 while True:
+ 
+    while game_state==1:
 
-    while game_state==0:
-        background.draw()
-        start.draw()
-
-
-        if mouse.is_button_pressed(1):
-            if mouse.is_over_object(start):
-                game_state = 1
-
-        window.update()
-
-    while game_state == 1:
         background.draw()
         bird_c.draw()
         llama_c.draw()
         elephant_c.draw()
         eye.draw()
+        bird.draw()
+        llama.draw()
+        elephant.draw()
 
-        for i in range(wave1):
-            if t >= 1:
-                t = 0
-                b1 = Sprite("b1.png")
-                bubbles1 = []
-                b2 = Sprite("b2.png")
-                b3 = Sprite("b3.png")
-                b1.set_position(window.width / 21 - b1.width, 4 * window.height / 21)
-                bubbles1.append(b1)
-            t = t + window.delta_time()
+        delta += window.delta_time()
+            
+        cria_bolhas(b1,bolhas1,1)
 
-        for i in range(len(bubbles1)):
-            bubbles1[i].draw()
+        for i in range(len(bolhas1)):
+            bolhas1[i].draw()
 
-        for i in range(len(bubbles1)):
-            bubbles1[i].x = bubbles1[i].x + (speed_x * window.delta_time())
+        if delta > 3: 
+            cria_bolhas(b2,bolhas2,2)
+            for i in range(len(bolhas2)):
+                bolhas2[i].draw()
 
-            if bubbles1[i].x < 3 * window.width / 21:
-                bubbles1[i].x = bubbles1[i].x + (speed_x * window.delta_time())
-            elif bubbles1[i].x >= 3 * window.width / 21:
-                speed_x = 0
-                bubbles1[i].y = bubbles1[i].y + (speed_y * window.delta_time())
-            elif bubbles1[i].y > 13 * window.height / 21:
-                speed_x = 10
-                speed_y = 0
-            elif bubbles1[i].x >= 10.75 * window.width / 21:
-                speed_x = 0
-                speed_y = -10
-                if bubbles1[i].y <= 9.5 * window.height / 21:
-                    speed_y = 0
-                    speed_x = 10
-            elif bubbles1[i].x >= 14.8 * window.width / 21:
-                speed_x = 0
-                speed_y = 10
-            elif bubbles1[i].y >= 16.5 * window.height / 21:
-                speed_x = 10
-                speed_y = 0
-
-        if mouse.is_button_pressed(1):
-            if mouse.is_over_object(bird_c):
-                h=2
-        if h==2:
-            if mouse.is_button_pressed(1):
-                bird.set_position(mouse.get_position()[0], mouse.get_position()[1])
-            bird.draw()
-
-        if teclado.key_pressed("ESC"):
-            game_state=0
+        if delta > 5: 
+            cria_bolhas(b3,bolhas3,3)
+            for i in range(len(bolhas3)):
+                bolhas3[i].draw()
+        
+        if keyboard.key_pressed("ESC"):
+            window.close()
 
         window.update()
